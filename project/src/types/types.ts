1edff76b1dcad3bd219aa6ce4e7e60b93c751eb9
Ivan Davydov <imdavydov@outlook.com>
2022-10-22 21:06:26 +0300

    Создает типы данных фильмов и комментариев, добавляет их моки

--- project/src/types/types.ts
+++ project/src/types/types.ts
@@ -11,3 +11,38 @@ export type HomePropsType = {
 export type AppPropsType = {
   home: HomePropsType;
 }
+
+export type Movie = {
+  id: number;
+  name: string;
+  posterImage: string;
+  previewImage: string;
+  backgroundImage: string;
+  backgroundColor: string;
+  videoLink: string;
+  previewVideoLink: string;
+  description: string;
+  rating: number;
+  scoresCount: number;
+  director: string;
+  starring: string[];
+  runTime: number;
+  genre: string;
+  released: number;
+  isFavorite: boolean;
+}
+
+export type Movies = Movie[]
+
+export type Review = {
+  comment: string;
+  date: string;
+  id: number;
+  rating: number;
+  user: {
+    id: number;
+    name: string;
+  };
+}
+
+export type Reviews = Review[]
