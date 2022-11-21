5d153850601959ec6aa2d3b23fc82698d16f8631
Ivan Davydov <imdavydov@outlook.com>
2022-11-21 23:29:23 +0300

    Убирает ненужные селекторы

--- project/src/store/service/selectors.ts
+++ project/src/store/service/selectors.ts
@@ -1,7 +1,5 @@
-import {ServiceType, State} from '../../types/state';
+import {State} from '../../types/state';
 import {AuthorizationStatus, Namespace} from '../../const';
-
-export const getServiceData = (state: State): ServiceType => state[Namespace.Service];
 
 export const getLoadingStatus = (state: State): boolean => state[Namespace.Service].isDataLoading;
 export const getAuthStatus = (state: State): AuthorizationStatus => state[Namespace.Service].authStatus;
--- project/src/store/user/selectors.ts
+++ project/src/store/user/selectors.ts
@@ -5,10 +5,6 @@ import {MovieType} from '../../types/types';
 export const getUserData = (state: State): UserType => state[Namespace.User];
 
 export const getId = (state: State): number | null => state[Namespace.User].id;
-export const getName = (state: State): string | null => state[Namespace.User].name;
-export const getEmail = (state: State): string | null => state[Namespace.User].email;
-export const getToken = (state: State): string | null => state[Namespace.User].token;
-export const getAvatarUrl = (state: State): string | null => state[Namespace.User].avatarUrl;
 export const getMyList = (state: State): MovieType[] => state[Namespace.User].myList;
 
 
