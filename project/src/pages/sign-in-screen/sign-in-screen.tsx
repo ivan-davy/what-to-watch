59be1ee81420b63ea3b3d2a1831daab41d19304c
Ivan Davydov <imdavydov@outlook.com>
2022-11-21 22:58:11 +0300

    Исправляет поведение страницы при неправильном вводе формы входа

--- project/src/pages/sign-in-screen/sign-in-screen.tsx
+++ project/src/pages/sign-in-screen/sign-in-screen.tsx
@@ -1,6 +1,6 @@
 import Logo from '../../components/logo/logo';
 import Footer from '../../components/footer/footer';
-import {FormEvent, useEffect, useRef} from 'react';
+import {FormEvent, useEffect, useRef, useState} from 'react';
 import {useAppDispatch, useAppSelector} from '../../hooks/store-hooks';
 import {useNavigate} from 'react-router-dom';
 import {AuthDataType} from '../../types/types';
@@ -10,10 +10,21 @@ import {EMAIL_PATTERN, PASSWORD_PATTERN} from '../../regex';
 import {redirectToRouteAction} from '../../store/action';
 import {getAuthStatus} from '../../store/service/selectors';
 
+function getSignInErrorMessage(inputId: string): string {
+  if (inputId === 'user-email') {
+    return 'Please enter a valid email address';
+  }
+  if (inputId === 'user-password') {
+    return 'Please enter a valid password';
+  }
+  return '';
+}
+
 export default function SignInScreen(): JSX.Element {
   const loginRef = useRef<HTMLInputElement | null>(null);
   const passwordRef = useRef<HTMLInputElement | null>(null);
   const authStatus = useAppSelector(getAuthStatus);
+  const [errFieldId, setErrFieldId] = useState('');
 
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
@@ -25,6 +36,14 @@ export default function SignInScreen(): JSX.Element {
   const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
     if (loginRef.current !== null && passwordRef.current !== null) {
+      if (!EMAIL_PATTERN.test(loginRef.current?.value)) {
+        setErrFieldId('user-email');
+        return;
+      }
+      if (!PASSWORD_PATTERN.test(passwordRef.current?.value)) {
+        setErrFieldId('user-password');
+        return;
+      }
       onSubmit({
         login: loginRef.current.value,
         password: passwordRef.current.value,
@@ -47,16 +66,19 @@ export default function SignInScreen(): JSX.Element {
       </header>
 
       <div className="sign-in user-page__content">
-        <form onSubmit={handleSubmit} className="sign-in__form">
+        <form onSubmit={handleSubmit} className="sign-in__form" noValidate>
+          <div className="sign-in__message">
+            <p>{getSignInErrorMessage(errFieldId)}</p>
+          </div>
           <div className="sign-in__fields">
-            <div className="sign-in__field">
-              <input ref={loginRef} pattern={EMAIL_PATTERN} className="sign-in__input" type="email" placeholder="Email address" name="user-email"
+            <div className={`sign-in__field ${errFieldId === 'user-email' ? 'sign-in__field--error' : ''}`}>
+              <input ref={loginRef} className="sign-in__input" type="email" placeholder="Email address" name="user-email"
                 id="user-email" required
               />
               <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
             </div>
-            <div className="sign-in__field">
-              <input ref={passwordRef} pattern={PASSWORD_PATTERN} className="sign-in__input" type="password" placeholder="Password" name="user-password"
+            <div className={`sign-in__field ${errFieldId === 'user-password' ? 'sign-in__field--error' : ''}`}>
+              <input ref={passwordRef} className="sign-in__input" type="password" placeholder="Password" name="user-password"
                 id="user-password" required
               />
               <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
